3dbd6d65372969f32342fab7ff15c3479b61bc6f
ErikKalkoken <erik.kalkoken@gmail.com>
2022-02-08 19:12:06 +0100

    Replace xml converter with more flexible version

--- memberaudit/core/xml_converter.py
+++ memberaudit/core/xml_converter.py
@@ -1,7 +1,9 @@
-import ast
 import re
 
-from django.utils.safestring import mark_safe
+from bs4 import BeautifulSoup
+
+from django.core.exceptions import ValidationError
+from django.core.validators import URLValidator
 from eveuniverse.models import EveEntity
 
 from allianceauth.eveonline.evelinks import dotlan, evewho
@@ -12,72 +14,92 @@ from .. import __title__
 
 logger = LoggerAddTag(get_extension_logger(__name__), __title__)
 
-
-_font_regex = re.compile(
-    r'<font (?P<pre>.*?)(size="(?P<size>[0-9]{1,2})")? ?(color="#[0-9a-f]{2}(?P<color>[0-9a-f]{6})")?(?P<post>.*?)>'
-)
-_link_regex = re.compile(
-    r'<a href="(?P<schema>[a-zA-Z]+):(?P<first_id>\d+)((//|:)(?P<second_id>[0-9a-f]+))?">'
-)
+DEFAULT_TEXT_COLOR = "ffffffb3"  # RGBA in hex
 
 
-def _font_replace(font_match) -> str:
-    pre = font_match.group("pre")  # before the color attr
-    size = font_match.group("size")
-    color = font_match.group("color")  # the raw color (eg. 'ffffff')
-    post = font_match.group("post")  # after the color attr
-
-    if color is None or color == "ffffff":
-        color_attr = ""
-    else:
-        color_attr = f"color: #{color};"
-    if size is None:
-        size_attr = ""
-    else:
-        size_attr = f"font-size: {size}pt;"
-    return f'<span {pre}style="{color_attr} {size_attr}"{post}>'
+def is_string_an_url(url_string: str) -> bool:
+    """True if given string is an URL, else False"""
+    validate_url = URLValidator()
+    try:
+        validate_url(url_string)
+    except ValidationError:
+        return False
+    return True
 
 
-def _link_replace(link_match) -> str:
-    schema = link_match.group("schema")
-    first_id = int(link_match.group("first_id"))
-    second_id = link_match.group("second_id")
-    if schema == "showinfo":
-        if second_id is not None:
-            second_id = int(second_id)
-        if 1373 <= first_id <= 1386:  # Character
-            return f'<a href="{evewho.character_url(second_id)}" target="_blank">'
-        elif first_id == 5:  # Solar System
-            system_name = EveEntity.objects.resolve_name(second_id)
-            return f'<a href="{dotlan.solar_system_url(system_name)}" target="_blank">'
-        elif first_id == 2:  # Corporation
-            corp_name = EveEntity.objects.resolve_name(second_id)
-            return f'<a href="{dotlan.corporation_url(corp_name)}" target="_blank">'
-        elif first_id == 16159:  # Alliance
-            alliance_name = EveEntity.objects.resolve_name(second_id)
-            return f'<a href="{dotlan.alliance_url(alliance_name)}" target="_blank">'
-    return """<a href="javascript:showInvalidError();">"""
+def eve_xml_to_html(xml_doc: str) -> str:
+    """Converts Eve Online XML to HTML."""
+    xml_doc = _remove_loc_tag(xml_doc)
+    soup = BeautifulSoup(xml_doc, "html.parser")
+    _convert_font_tag(soup)
+    _convert_a_tag(soup)
+    return str(soup)
 
 
-def is_ascii(s):
-    return all(ord(c) < 128 for c in s)
+def _remove_loc_tag(xml: str) -> str:
+    xml = xml.replace("<loc>", "")
+    return xml.replace("</loc>", "")
 
 
-def eve_xml_to_html(xml: str) -> str:
-    x = _font_regex.sub(_font_replace, xml)
-    x = x.replace("</font>", "</span>")
-    x = _link_regex.sub(_link_replace, x)
-    # x = strip_tags(x)
-    if is_ascii(x):
-        x = bytes(x, "ascii").decode("unicode-escape")
-        # temporary fix to address u-bug in ESI endpoint
-        # workaround to address syntax error bug (#77)
-        # TODO: remove when fixed
-        if x.startswith("u'") and x.endswith("'"):
+def _convert_font_tag(soup):
+    for element in soup.find_all("font"):
+        element.name = "span"
+        styles = []
+        if "size" in element.attrs:
+            styles.append(f"font-size:{element['size']}")
+            del element["size"]
+        if "color" in element.attrs:
+            color_ccp = element["color"].replace("#", "")
             try:
-                x = ast.literal_eval(x)
-            except SyntaxError:
-                logger.warning("Failed to convert XML")
-                x = ""
+                hex_color = _ccp_color_to_rgba(color_ccp)
+            except ValueError:
+                hex_color = DEFAULT_TEXT_COLOR
+            styles.append(f"color:#{hex_color}")
+            del element["color"]
+        if styles:
+            element["style"] = ";".join(styles)
 
-    return mark_safe(x)
+
+def _ccp_color_to_rgba(ccp_color) -> str:
+    bytes = [ccp_color[i : i + 2] for i in range(0, len(ccp_color), 2)]
+    try:
+        return bytes[1] + bytes[2] + bytes[3] + bytes[0]
+    except KeyError:
+        raise ValueError from None
+
+
+def _convert_a_tag(soup: BeautifulSoup):
+    for element in soup.find_all("a"):
+        href = element["href"]
+        new_href = (
+            element["href"] if is_string_an_url(href) else _match_eve_entity(href)
+        )
+        if new_href:
+            element["href"] = new_href
+            element["target"] = "_blank"
+        else:
+            element["href"] = "#"
+
+
+def _match_eve_entity(href: str) -> str:
+    link_match = re.match(
+        r"(?P<schema>[a-zA-Z]+):(?P<type_id>\d+)\/\/(?P<entity_id>[0-9a-f]+)",
+        href,
+    )
+    if link_match and link_match.group("schema") == "showinfo":
+        type_id = int(link_match.group("type_id"))
+        entity_id = link_match.group("entity_id")
+        if entity_id is not None:
+            entity_id = int(entity_id)
+        if 1373 <= type_id <= 1386:  # Character
+            return evewho.character_url(entity_id)
+        elif type_id == 5:  # Solar System
+            system_name = EveEntity.objects.resolve_name(entity_id)
+            return dotlan.solar_system_url(system_name)
+        elif type_id == 2:  # Corporation
+            corp_name = EveEntity.objects.resolve_name(entity_id)
+            return dotlan.corporation_url(corp_name)
+        elif type_id == 16159:  # Alliance
+            alliance_name = EveEntity.objects.resolve_name(entity_id)
+            return dotlan.alliance_url(alliance_name)
+    return None
---
+++ memberaudit/tests/test_core_xml_converter.py
@@ -0,0 +1,165 @@
+from unittest.mock import Mock, patch
+
+from allianceauth.eveonline.evelinks import dotlan, evewho
+from app_utils.testing import NoSocketsTestCase
+
+from ..core.xml_converter import eve_xml_to_html
+from .testdata.esi_client_stub import load_test_data
+from .testdata.load_entities import load_entities
+from .testdata.load_eveuniverse import load_eveuniverse
+
+MODULE_PATH = "memberaudit.core.xml_converter"
+
+
+class TestXMLConversion(NoSocketsTestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        load_eveuniverse()
+        load_entities()
+
+    def test_convert_eve_xml_alliance(self):
+        """can convert an alliance link in CCP XML to HTML"""
+        with patch(
+            "eveuniverse.models.EveEntity.objects.resolve_name",
+            Mock(return_value="An Alliance"),
+        ):
+            result = eve_xml_to_html(
+                load_test_data()
+                .get("Mail")
+                .get("get_characters_character_id_mail_mail_id")
+                .get("2")
+                .get("body")
+            )
+            self.assertTrue(result.find(dotlan.alliance_url("An Alliance")) != -1)
+
+    def test_convert_eve_xml_character(self):
+        """can convert a character link in CCP XML to HTML"""
+        result = eve_xml_to_html(
+            load_test_data()
+            .get("Mail")
+            .get("get_characters_character_id_mail_mail_id")
+            .get("2")
+            .get("body")
+        )
+        self.assertTrue(result.find(evewho.character_url(1001)) != -1)
+
+    def test_convert_eve_xml_corporation(self):
+        """can convert a corporation link in CCP XML to HTML"""
+        with patch(
+            "eveuniverse.models.EveEntity.objects.resolve_name",
+            Mock(return_value="A Corporation"),
+        ):
+            result = eve_xml_to_html(
+                load_test_data()
+                .get("Mail")
+                .get("get_characters_character_id_mail_mail_id")
+                .get("2")
+                .get("body")
+            )
+            self.assertTrue(result.find(dotlan.alliance_url("A Corporation")) != -1)
+
+    def test_convert_eve_xml_solar_system(self):
+        """can convert a solar system link in CCP XML to HTML"""
+        with patch(
+            "eveuniverse.models.EveEntity.objects.resolve_name",
+            Mock(return_value="Polaris"),
+        ):
+            result = eve_xml_to_html(
+                load_test_data()
+                .get("Mail")
+                .get("get_characters_character_id_mail_mail_id")
+                .get("2")
+                .get("body")
+            )
+            self.assertTrue(result.find(dotlan.solar_system_url("Polaris")) != -1)
+
+    # def test_convert_bio_1(self):
+    #     """can convert a bio includes lots of non-ASCII characters and handle the u-bug"""
+    #     with patch(
+    #         "eveuniverse.models.EveEntity.objects.resolve_name",
+    #         Mock(return_value="An Alliance"),
+    #     ):
+    #         result = eve_xml_to_html(
+    #             load_test_data()
+    #             .get("Character")
+    #             .get("get_characters_character_id")
+    #             .get("1002")
+    #             .get("description")
+    #         )
+    #         self.assertIn(
+    #             "Zuverlässigkeit, Eigeninitiative, Hilfsbereitschaft, Teamfähigkeit",
+    #             result,
+    #         )
+    #         self.assertNotEqual(result[:2], "u'")
+
+    # def test_convert_bio_2(self):
+    #     """can convert a bio that resulted in a syntax error (#77)"""
+    #     with patch(
+    #         "eveuniverse.models.EveEntity.objects.resolve_name",
+    #         Mock(return_value="An Alliance"),
+    #     ):
+    #         try:
+    #             result = eve_xml_to_html(
+    #                 load_test_data()
+    #                 .get("Character")
+    #                 .get("get_characters_character_id")
+    #                 .get("1003")
+    #                 .get("description")
+    #             )
+    #         except Exception as ex:
+    #             self.fail(f"Unexpected exception was raised: {ex}")
+
+    #         self.assertNotEqual(result[:2], "u'")
+
+
+class TestXMLConversion2(NoSocketsTestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        load_eveuniverse()
+        load_entities()
+
+    def test_should_convert_font_tag(self):
+        input = """<font size="13" color="#b3ffffff">Character</font>"""
+        expected = """<span style="font-size:13;color:#ffffffb3">Character</span>"""
+        self.assertHTMLEqual(eve_xml_to_html(input), expected)
+
+    def test_should_remove_loc_tag(self):
+        input = """<loc>Character</loc>"""
+        expected = """Character"""
+        self.assertHTMLEqual(eve_xml_to_html(input), expected)
+
+    def test_add_target_to_normal_links(self):
+        input = """<a href="http://www.google.com" target="_blank">https://www.google.com</a>"""
+        self.assertHTMLEqual(eve_xml_to_html(input), input)
+
+    def test_should_convert_character_link(self):
+        input = """<a href="showinfo:1376//1001">Bruce Wayne</a>"""
+        expected = """<a href="https://evewho.com/character/1001" target="_blank">Bruce Wayne</a>"""
+        self.assertHTMLEqual(eve_xml_to_html(input), expected)
+
+    def test_should_convert_corporation_link(self):
+        input = """<a href="showinfo:2//2001">Wayne Technologies</a>"""
+        expected = """<a href="http://evemaps.dotlan.net/corp/Wayne_Technologies" target="_blank">Wayne Technologies</a>"""
+        self.assertHTMLEqual(eve_xml_to_html(input), expected)
+
+    def test_should_convert_alliance_link(self):
+        input = """<a href="showinfo:16159//3001">Wayne Enterprises</a>"""
+        expected = """<a href="http://evemaps.dotlan.net/alliance/Wayne_Enterprises" target="_blank">Wayne Enterprises</a>"""
+        self.assertHTMLEqual(eve_xml_to_html(input), expected)
+
+    def test_should_convert_solar_system_link(self):
+        input = """<a href="showinfo:5//30004984">Abune</a>"""
+        expected = """<a href="http://evemaps.dotlan.net/system/Abune" target="_blank">Abune</a>"""
+        self.assertHTMLEqual(eve_xml_to_html(input), expected)
+
+    def test_should_disable_unknown_types(self):
+        input = """<a href="showinfo:666//30004984">Abune</a>"""
+        expected = """<a href="#">Abune</a>"""
+        self.assertHTMLEqual(eve_xml_to_html(input), expected)
+
+    def test_should_disable_unknown_links(self):
+        input = """<a href="unknown">Abune</a>"""
+        expected = """<a href="#">Abune</a>"""
+        self.assertHTMLEqual(eve_xml_to_html(input), expected)
