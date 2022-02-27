0c12fea48787f69a775b752c508359ef4d047adc
Peter Pfeufer <info@ppfeufer.de>
2022-02-27 15:12:41 +0100

    [FIX] Cannot import name 'ugettext_lazy' from 'django.utils.translation'
    Fixes #109

--- memberaudit/app_settings.py
+++ memberaudit/app_settings.py
@@ -1,5 +1,5 @@
 from django.utils.text import slugify
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from app_utils.django import clean_setting
 
