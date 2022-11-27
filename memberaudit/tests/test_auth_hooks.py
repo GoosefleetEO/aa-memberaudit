3797b607fdfe0546371387f21f8c92c42ac7682d
Peter Pfeufer <info@ppfeufer.de>
2022-11-28 00:41:32 +0100

    2 of 3 tests working, time to get some sleep :-)

--- memberaudit/tests/test_auth_hooks.py
+++ memberaudit/tests/test_auth_hooks.py
@@ -6,7 +6,7 @@ Test auth_hooks
 from http import HTTPStatus
 
 # Django
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.urls import reverse
 
 from app_utils.testing import create_fake_user
@@ -25,10 +25,10 @@ class TestHooks(TestCase):
 
         super().setUpClass()
 
-        # User cannot access bulletins
+        # User cannot access memberaudit
         cls.user_1001 = create_fake_user(1001, "Peter Parker")
 
-        # User can access bulletins
+        # User can access memberaudit
         cls.user_1002 = create_fake_user(
             1002, "Bruce Wayne", permissions=["memberaudit.basic_access"]
         )
@@ -49,33 +49,43 @@ class TestHooks(TestCase):
         """
 
         self.client.force_login(self.user_1002)
+
         response = self.client.get(reverse("authentication:dashboard"))
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertContains(response, "Member Audit", html=True)  # Link name
-        self.assertContains(response, "/member-audit/", html=True)  # Link slug
         self.assertContains(response, self.html_menu, html=True)
 
-        # Test custom app name
-        with self.settings(MEMBERAUDIT_APP_NAME="これが監査です"):
-            response = self.client.get(reverse("authentication:dashboard"))
+    @override_settings(MEMBERAUDIT_APP_NAME="これが監査です")
+    def test_render_hook_success_with_custom_app_name(self):
+        """
+        Test should show the link to the app in the navigation to user with access with
+        a custom app name. これが監査です is Japanese for "This is the audit". This
+        should result in "koregajian-cha-desu" as app slug
+        :return:
+        :rtype:
+        """
 
-            expected_custom_html_menu = """
-                <li>
-                    <a class href="/koregajian-cha-desu/">
-                        <i class="far fa-address-card fa-fw fa-fw"></i> これが監査です
-                    </a>
-                </li>
-            """
+        self.client.force_login(self.user_1002)
 
-            self.assertEqual(response.status_code, HTTPStatus.OK)
-            self.assertContains(response, "これが監査です", html=True)
-            self.assertContains(response, "/koregajian-cha-desu/", html=True)
-            self.assertContains(response, expected_custom_html_menu, html=True)
+        response = self.client.get(reverse("authentication:dashboard"))
+
+        expected_custom_html_menu = """
+            <li>
+                <a class href="/koregajian-cha-desu/">
+                    <i class="far fa-address-card fa-fw fa-fw"></i> これが監査です
+                </a>
+            </li>
+        """
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        # self.assertContains(response, "これが監査です")
+        # self.assertContains(response, "/koregajian-cha-desu/")
+        self.assertContains(response, expected_custom_html_menu, html=True)
 
     def test_render_hook_fail(self):
         """
-        Test should not show the link to the app in the navigation to user without access
+        Test should not show the link to the app in the
+        navigation to user without access
         :return:
         :rtype:
         """
